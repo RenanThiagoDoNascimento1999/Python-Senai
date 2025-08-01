6996b78a160f268497fdd63361ee48dd7aa6fd4b
RenanThiagoDoNascimento1999 <renanthiago1998@gmail.com>
2025-07-31 21:52:33 -0300

    Add files via upload

---
+++ e-ecommecer2.py
@@ -0,0 +1,89 @@
+# e_commerce = {
+
+# 1:{
+# 'a':20.00,
+# 'b':30.00,
+# 'c':10.00
+# },
+# 2:{
+# 'a':20.00,
+# 'b':30.00,
+# 'c':10.00
+# },
+# 3:{
+# 'a':20.00,
+# 'b':30.00,
+# 'c':10.00
+# }
+# }
+
+# carrinho = []
+# meu_total = []
+
+# print('Digite o id da seção:livros 1 - tablets 2  - fones 3')
+
+# secao = int(input('escolha a sessão: ')) 
+# p1 = input('Digite')
+
+# carrinho.append(p1)
+# print(carrinho)
+
+# secao2 = int(input('escolha a sessão: ')) 
+# p2 = input('Digite')
+# carrinho.append(p2)
+# print(carrinho)
+
+# v1 = e_commerce[secao][p1]
+# v2 = e_commerce[secao2][p2]
+
+# meu_total.extend([v1,v2])
+
+# soma = sum(meu_total)
+
+# print('Carrinho', carrinho)
+# print('R$', soma)
+
+
+# e_commerce = {
+# 'livros':20,
+# 'tablets':10,
+# 'fone':50
+# }
+
+
+# carrinho = []
+# meu_total = []
+
+# secao = input('escolha a sessão: ') 
+# carrinho.append(secao)
+# print(carrinho)
+
+# meu_total.append(e_commerce[secao])
+
+# secao2 = input('escolha a sessão: ')
+# carrinho.append(secao2)
+# print(carrinho)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
