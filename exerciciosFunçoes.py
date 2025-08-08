43e273828bc01a73acac1b57442e00be583978a6
RenanThiagoDoNascimento1999 <renanthiago1998@gmail.com>
2025-08-07 21:57:33 -0300

    Add files via upload

---
+++ exerciciosFunçoes.py
@@ -0,0 +1,77 @@
+def numeros():
+    numeros = int (input("DIGITA O NUMERO:"))
+    numeros = int (input("DIGITA O NUMERO:"))
+    if numeros % 2 == 0:
+        print("PAR")
+    else:
+       print("IMPAR")
+
+
+numeros()  
+
+numeros1 = int (input("DIGITE O NUMERO:"))
+numeros2 = int (input ("DIGITE O NUMERO:"))
+numeros3 = int (input("DIGITE NUMERO: "))
+def soma ():
+    soma = numeros1 + numeros2 + numeros3
+    print("O RESULTADO DA SOMA É:",soma)
+
+soma()
+
+
+numerosR1 = int (input("DIGITE O NUMERO: "))
+numerosR2 = int (input("DIGITE O NUMERO:"))
+
+def potencia ():
+    potencia = numerosR1 ** numerosR2
+    print("O RESULTADO  DO VALOR ELEVADO É:",potencia)
+
+potencia()
+
+
+anos = input("DIGITA SEU ANO:")
+
+
+def anos(anos_que_nasceu,anos):
+   D = anos_que_nasceu - anos
+   return anos 
+
+
+if anos and 18:
+    print("MAIOR DE IDADE")
+else:
+    print("MENOR DE IDADE")
+
+print(anos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
