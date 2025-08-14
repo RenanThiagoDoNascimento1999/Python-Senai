ad29803df899404fb94f74a53d1a65fc9a00ad4a
RenanThiagoDoNascimento1999 <renanthiago1998@gmail.com>
2025-08-13 21:00:07 -0300

    Add files via upload

---
+++ MOVIMENTO.py
@@ -0,0 +1,63 @@
+import pygame
+import sys
+
+pygame.init()
+
+width = 700
+height = 500
+
+screen = pygame.display.set_mode((width, height)) 
+
+quandrado =  pygame.Rect(350,200, 150,70)
+rectangulo2 = pygame.Rect(10,150, 50,50)
+
+clock =  pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           pygame.quit() 
+           sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                quandrado.move_ip([10,0])
+            if event.key == pygame.K_LEFT:
+                quandrado.move_ip([-10,0])
+            if event.key == pygame.K_UP:
+                quandrado.move_ip([0,-10])
+            if event.key == pygame.K_DOWN:
+                quandrado.move_ip([0,10])     
+
+        
+        screen.fill('red')
+        pygame.draw.rect(screen,('green'), quandrado)
+        pygame.draw.rect(screen,('white'), rectangulo2)
+        pygame.display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
